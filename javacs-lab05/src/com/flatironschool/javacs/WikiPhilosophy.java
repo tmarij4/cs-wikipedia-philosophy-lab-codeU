59febeac1df1dcbef12cb0afe54adadd7a745f8a
tmarij4 <Tessa@Tessas-MacBook-Pro.local>
2016-05-13 14:35:08 -0400

    Done
    Passed Test

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -27,25 +27,61 @@ public class WikiPhilosophy {
 	 * @param args
 	 * @throws IOException
 	 */
-	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
-
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
-
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
+    public static void main(String[] args) throws IOException {
+        
+        boolean success = false, running = true;
+        String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+        List<String> store = new ArrayList<String>();
+        
+        while (!success && running){
+            
+            Elements paragraphs = wf.fetchWikipedia(url);
+            Element firstPara = paragraphs.get(0);
+            String help = visit(url, firstPara);
+            if (help.equals(null)){
+                running = false;
+            }else{
+                store.add(help);
+                if (help.equals("https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy")){
+                    success = true;
+                }
+                url = help;
+            }
         }
-
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
-	}
+    }
+    
+    public static String visit(String current, Element para){
+        Iterable<Node> iter = new WikiNodeIterable(para);
+        for (Node node: iter) {
+            if (node instanceof Element) {
+                Element ele = (Element) node;
+                if (valid(ele, current)){
+                    String url = node.attr("href");
+                    return url;
+                }
+            }
+        }
+        return null;
+    }
+    
+    public static boolean valid(Element ele, String current){
+        
+        //Italics Check
+        for (Element start = ele; start != null; start = start.parent()){
+            if (start.tagName().equals("i") || start.tagName().equals("em")){
+                return false;
+            }
+        }
+        
+        //Starts with strange letters
+        if (!ele.attr("href").startsWith("/wiki/Help:")){
+            return false;
+        }
+        
+        //Is the current page
+        if (ele.attr("href").equals(current)){
+            return false;
+        }
+        return true;
+    }
 }
